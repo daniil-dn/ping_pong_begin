9713da465a33b7d7a8be0e12a00b2726298e9e22
daniildn <dosindaniilm@gmail.com>
2021-12-27 00:10:21 +0300

    completed functions for paddles control

--- main.py
+++ main.py
@@ -24,10 +24,52 @@ paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
 
-
 # Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.shapesize(stretch_wid=2, stretch_len=2)
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 2
+ball.dy = 2
 
+
+# Functions
+
+def paddle_a_up() -> None:
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+
+def paddle_a_down() -> None:
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+
+def paddle_b_up() -> None:
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+
+def paddle_b_down() -> None:
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+
+# Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
 
 # main game loop
 while True:
     wn.update()
+
