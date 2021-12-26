a2fb238a7645ef8e10652c823b7f2cc7be0b1b20
daniildn <dosindaniilm@gmail.com>
2021-12-27 02:16:33 +0300

    created a function for sound for any platform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import turtle
 import os
+import platform
 
 wn = turtle.Screen()
 wn.title('pong by Daniil_Dn')
@@ -37,8 +38,8 @@ ball.color("white")
 ball.shapesize(stretch_wid=2, stretch_len=2)
 ball.penup()
 ball.goto(0, 0)
-ball.dx = 2
-ball.dy = 2
+ball.dx = 3
+ball.dy = 3
 
 # Pen
 pen = turtle.Turtle()
@@ -52,6 +53,15 @@ pen.write("Player A: 0 Player B: 0", align='center', font=("Courier", 24, "norma
 
 # Functions
 
+def play_sound(file_name):
+    platform_sys = platform.system()
+    if platform_sys == "Linux":
+        os.system(f"aplay {file_name}&")
+    elif platform_sys == "Darwin":
+        os.system(f"afplay {file_name}&")
+    elif platform_sys == "Windows":
+        import winsound
+        winsound.PlaySound(file_name, winsound.SND_ASYNC)
 
 
 def paddle_a_up() -> None:
@@ -87,47 +97,52 @@ wn.onkeypress(paddle_b_down, "Down")
 
 # main game loop
 while True:
-    wn.update()
+    try:
+        wn.update()
 
-    # Move the ball
-    ball.setx(ball.xcor() + ball.dx)
-    ball.sety(ball.ycor() + ball.dy)
+        # Move the ball
+        ball.setx(ball.xcor() + ball.dx)
+        ball.sety(ball.ycor() + ball.dy)
 
-    # Border collision
-    if ball.ycor() > 290:
-        ball.sety(290)
-        ball.dy *= -1
-        os.system('afplay b.wav&')
+        # Border collision
+        if ball.ycor() > 290:
+            ball.sety(290)
+            ball.dy *= -1
+            play_sound('b.wav')
 
-    if ball.ycor() < -290:
-        ball.sety(-290)
-        ball.dy *= -1
-        os.system('afplay b.wav&')
+        if ball.ycor() < -290:
+            ball.sety(-290)
+            ball.dy *= -1
+            play_sound('b.wav')
 
-    if ball.xcor() > 390:
-        score_a += 1
-        pen.clear()
-        pen.write(f"Player A: {score_a} Player B: {score_b}", align='center', font=("Courier", 24, "normal"))
-        ball.goto(0, 0)
-        ball.dx *= -1
-        os.system('afplay b.wav&')
-    if ball.xcor() < -390:
-        score_b += 1
-        pen.clear()
-        pen.write(f"Player A: {score_a} Player B: {score_b}", align='center', font=("Courier", 24, "normal"))
-        ball.goto(0, 0)
-        ball.dx *= -1
-        os.system('afplay b.wav&')
+        if ball.xcor() > 390:
+            score_a += 1
+            pen.clear()
+            pen.write(f"Player A: {score_a} Player B: {score_b}", align='center', font=("Courier", 24, "normal"))
+            ball.goto(0, 0)
+            ball.dx *= -1
+            play_sound('b.wav')
+        if ball.xcor() < -390:
+            score_b += 1
+            pen.clear()
+            pen.write(f"Player A: {score_a} Player B: {score_b}", align='center', font=("Courier", 24, "normal"))
+            ball.goto(0, 0)
+            ball.dx *= -1
+            play_sound('b.wav')
 
-    # Paddle and ball collisions
-    if (ball.xcor() > 340 and ball.xcor() < 350) and (
-            ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50):
-        ball.setx(340)
-        ball.dx *= -1
-        os.system('afplay b.wav&')
+        # Paddle and ball collisions
+        if (ball.xcor() > 340 and ball.xcor() < 350) and (
+                ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50):
+            ball.setx(340)
+            ball.dx *= -1
+            play_sound('b.wav')
 
-    if (ball.xcor() < -340 and ball.xcor() > -350) and (
-            ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50):
-        ball.setx(-340)
-        ball.dx *= -1
-        os.system('afplay b.wav&')
+        if (ball.xcor() < -340 and ball.xcor() > -350) and (
+                ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50):
+            ball.setx(-340)
+            ball.dx *= -1
+            play_sound('b.wav')
+
+    except Exception as error:
+        print('smth went wrong: \n', error)
+        break
